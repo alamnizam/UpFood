a138d28362edccf436f81c10a78f3e4b8e2d6d7e
nalamqas <alamnizam1992@gmail.com>
2025-03-24 16:36:22 +0530

    Refactor: Centralize default configurations and update dependencies
    This commit centralizes the default configurations for Android projects in the top-level `build.gradle.kts` file. It defines a `defaultConfig` function that sets the `compileSdkVersion`, `minSdk`, `targetSdk`, `testInstrumentationRunner`, `consumerProguardFiles`, and vector drawables settings. It also configures the `compileOptions` to use Java 17 and sets `isIncludeAndroidResources` for unit tests.
    
    A new `applyDefaultConfig` function is added to apply the `defaultConfig` to different plugin types (`AppPlugin`, `LibraryPlugin`, `JavaPlugin`).
    The `subprojects` block iterates through subprojects, applying default configuration and settings the jvm target to 17 and added free arguments for kotlin compiler.
    The `app/build.gradle.kts` file is updated to remove the redundant `minSdk`, `targetSdk`, `testInstrumentationRunner`, `compileOptions` and `kotlinOptions` configuration.
    The `gradle/libs.versions.toml` file is updated, reorganizing and adding new dependencies.
    Dependencies are divided into sections: core, compose, Junit test and espresso.
    Update versions for core, junit, espresso, compose, and add new dependencies to the `libs.versions.toml` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,8 @@ android {
 
     defaultConfig {
         applicationId = "com.codeturtle.upfood"
-        minSdk = 24
-        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -27,13 +23,6 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
     buildFeatures {
         compose = true
     }
@@ -41,19 +30,26 @@ android {
 
 dependencies {
 
+    //core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
+    //Compose
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    debugImplementation(libs.androidx.ui.test.manifest)
+    debugImplementation(libs.androidx.ui.tooling)
+
+    //Junit testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Espresso
+    androidTestImplementation(libs.androidx.espresso.core)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,77 @@
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.LibraryExtension
+import com.android.build.gradle.LibraryPlugin
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+}
+
+fun BaseExtension.defaultConfig(){
+    compileSdkVersion(35)
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 35
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+        vectorDrawables{
+            useSupportLibrary = true
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+}
+
+fun PluginContainer.applyDefaultConfig(project: Project){
+    whenPluginAdded{
+        when(this){
+            is AppPlugin -> {
+                project.extensions.getByType<AppExtension>().apply {
+                    defaultConfig()
+                }
+            }
+            is LibraryPlugin -> {
+                project.extensions.getByType<LibraryExtension>().apply {
+                    defaultConfig()
+                }
+            }
+            is JavaPlugin ->{
+                project.extensions.getByType<JavaPluginExtension>().apply{
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+        }
+    }
+}
+
+subprojects {
+    project.plugins.applyDefaultConfig(project)
+
+    tasks.withType<KotlinCompile>{
+        compilerOptions {
+            jvmTarget.set(JVM_17)
+            freeCompilerArgs.addAll(
+                listOf(
+                    "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+                )
+            )
+        }
+    }
 }
