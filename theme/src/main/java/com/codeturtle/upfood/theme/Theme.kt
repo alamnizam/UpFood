04a31d478e65164edc2cc4de943f6a7495c9ed9a
nalamqas <alamnizam1992@gmail.com>
2025-03-25 21:55:37 +0530

    feat: Disable dynamic color in theme module
    This commit disables dynamic color theming in the `theme` module by default.
    The `dynamicColor` parameter in the `UpFoodTheme` composable function is set to `false`, ensuring that static color schemes are used instead of dynamically generated colors based on the user's wallpaper.
    This is done to use static colors instead of dynamic, as per project requirements.

--- theme/src/main/java/com/codeturtle/upfood/theme/Theme.kt
+++ theme/src/main/java/com/codeturtle/upfood/theme/Theme.kt
@@ -260,8 +260,8 @@ val unspecified_scheme = ColorFamily(
 fun UpFoodTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable() () -> Unit
+    dynamicColor: Boolean = false,
+    content: @Composable () -> Unit
 ) {
   val colorScheme = when {
       dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
