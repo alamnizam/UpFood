06a75898715a6cb414589afcee121744d6e408fb
nalamqas <alamnizam1992@gmail.com>
2025-03-25 21:29:24 +0530

    feat: Introduce UiText for dynamic and resource-based text
    This commit introduces a new `UiText` sealed class within the `theme` module to handle both dynamic strings and string resources.
    
    -   **UiText Class**: The `UiText` sealed class is created with two data classes:
        -   `DynamicString`: Holds a string value for dynamic text.
        -   `StringResource`: Holds a string resource ID and optional arguments.
    -   **String Conversion**: Provides methods to convert `UiText` to a `String`:
        -   `asString(context: Context)`: Converts `UiText` to a string using the provided `Context`.
        -   `asString(): @Composable String`: Converts `UiText` to a string within a Composable function, using string resources.
    -   **File Creation**: The `UiText.kt` file is added to `theme/src/main/java/com/codeturtle/upfood/theme/utils/`.

---
+++ theme/src/main/java/com/codeturtle/upfood/theme/utils/UiText.kt
@@ -0,0 +1,32 @@
+package com.codeturtle.upfood.theme.utils
+
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+
+sealed class UiText {
+    data class DynamicString(
+        val value: String
+    ): UiText()
+
+    class StringResource(
+        @StringRes val id: Int,
+        vararg val args: Any
+    ): UiText()
+
+    fun asString(context: Context): String {
+        return when(this){
+            is DynamicString -> value
+            is StringResource -> context.getString(id, *args)
+        }
+    }
+
+    @Composable
+    fun asString(): String {
+        return when(this){
+            is DynamicString -> value
+            is StringResource -> stringResource(id, *args)
+        }
+    }
+}
