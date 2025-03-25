fb2a3337a5ad4ce89b778b0439d72545ecc9f2ec
nalamqas <alamnizam1992@gmail.com>
2025-03-25 22:03:16 +0530

    feat: Implement a primary button with an arrow icon
    This commit introduces a new `PrimaryButton` composable within the `theme` module.
    The `PrimaryButton` is a filled button, designed for primary actions and it contains:
    - A text label.
    - A trailing arrow icon to provide visual guidance.
    - The button has a rounded corner shape.
    - The button's arrow is aligned vertically in the center of the button and have a fixed size.

--- theme/src/main/java/com/codeturtle/upfood/theme/components/Buttons.kt
+++ theme/src/main/java/com/codeturtle/upfood/theme/components/Buttons.kt
@@ -2,10 +2,9 @@ package com.codeturtle.upfood.theme.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowForward
@@ -15,6 +14,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -49,9 +49,11 @@ fun AppButton(
                     textAlign = TextAlign.Center,
                 )
             )
-            Spacer(modifier = Modifier.width(5.dp))
             Icon(
-                Icons.AutoMirrored.Outlined.ArrowForward,
+                modifier = Modifier.padding(start = 10.dp)
+                    .size(15.dp)
+                    .align(Alignment.CenterVertically),
+                imageVector = Icons.AutoMirrored.Outlined.ArrowForward,
                 contentDescription = stringResource(R.string.arrow_forward)
             )
         }
